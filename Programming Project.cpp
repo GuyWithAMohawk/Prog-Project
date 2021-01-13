a69b42fdbd1b3af18415bce8423119c76cb35cea
GuyWithAMohawk <stillbreathing32@gmail.com>
2021-01-14 07:58:32 +0800

    Dropping the main .cpp

---
+++ Programming Project.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+char gameboard[10] = {'o', 'x'} // game board chars. ABCDDEFG or 1234?
+void board();
+void save();
+int winning();
+
+int main()
+{
+	int player = 1,s,choice;
+	
+	char mark;	
+	
+	return 0;
+}
+
+
+// Functions for game status
+
+
+int winning() //Func. for winning
+{
+	// use an if and else if statement here.
+	// try smth like box[1] == box[2] && box[3]
+	// or set a number to each slot like box[1] = 1 and hitting 3 boxes = 6
+	// 1 + 2 + 3 = 6 = run func winning
+}
+
+
+void board() //Func for drawing the board. use ____ ||| and box[1] vars.
+
+cout << 
+
+void save()
+// on new save create file with slot value = 1/2/3
+// on load save, load file 1/2/3
+// on save file, save 1/2/3 value. overwrite old save value.
+// TO DO LIST
+
+// func winning
+// func board
+// func save
